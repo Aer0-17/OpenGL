11dd2940847c9e1b6077a8889c120512015c0fd3
kun.luo <953375842@qq.com>
2022-10-28 22:20:16 +0800

    少了解绑操作

--- src/Application.cpp
+++ src/Application.cpp
@@ -229,7 +229,8 @@ int main(void)
         glUniform4f(location, 1.0f, 0.0f, 0.0f, 1.0f);
 
         //unbind
-        glBindVertexArray(0);
+        //glBindVertexArray(0);
+        va.UnBind();
         glUseProgram(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
